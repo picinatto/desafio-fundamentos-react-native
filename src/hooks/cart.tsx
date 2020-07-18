ba20ffa7d6ba69ed9b45467d13cd9504141e7b1c
Ricardo <ederli.picinatto@gmail.com>
2020-07-18 15:33:53 -0300

    Started add to cart

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -37,7 +37,7 @@ const CartProvider: React.FC = ({ children }) => {
   }, []);
 
   const addToCart = useCallback(async product => {
-    // TODO ADD A NEW ITEM TO THE CART
+    console.log(product);
   }, []);
 
   const increment = useCallback(async id => {
